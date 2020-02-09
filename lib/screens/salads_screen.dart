d0c4b5120e07ca33c29d3123c69599c3de0bfddd
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-02-09 17:23:11 +0100

    Addition of a drawer menu

---
+++ lib/screens/salads_screen.dart
@@ -0,0 +1,8 @@
+class SaladsScreen extends StatelessWidget {
+  const SaladsScreen({}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A));
+  }
+}
